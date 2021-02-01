af40b41da6fd5bffa51982c23f851b7308c1c49f
shahnewaz601 <shahnewaz601@gmail.com>
2021-02-01 17:10:25 +0600

    Destructure, Object, array, destructure complex object

---
+++ destructure .js
@@ -0,0 +1,21 @@
+//====Object====///
+// const person = {name: "Jack William", age: 17, gfName: "Ema Watson", height: 45, job: "Facebooker", Phone: "0171717171"};
+
+// const gfName = person.gfName;
+// const phone = person.Phone;
+
+// console.log(gfName, phone);
+// console.log(gfName, phone);
+// console.log(gfName, phone);
+// console.log(gfName, phone);
+
+//Another Example
+const person2 = {name2: "Jack William", age2: 17, gfName2: "Ema Watson", height2: 45, job2: "Facebooker", phone2: "0171717171"};
+
+const {phone2} = person2;
+const gfName2 = person2.gfName2;
+
+console.log(gfName2, phone2);
+console.log(gfName2, phone2);
+console.log(gfName2, phone2);
+console.log(gfName2, phone2);
