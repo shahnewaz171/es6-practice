b11531c887cb551bbfe7822d0e87fd34af183ade
shahnewaz601 <shahnewaz601@gmail.com>
2021-02-01 16:39:38 +0600

    Inheritance, extends class, super, class method

--- Inheritance.js
+++ Inheritance.js
@@ -1,19 +1,44 @@
 //=====Inheritance====//
+// class Parent{
+//     constructor(){
+//         this.fatherName = "Schwarzenegger";
+//     }
+// }
+
+// class Child extends Parent{
+//     constructor(name){
+//         super();
+//         this.name = name;
+//     }
+//     getFullName(){
+//         return this.name + this.fatherName;
+//     }
+// }
+// const baby = new Child("Arnold");
+// const baby2 = new Child("Tom");
+// console.log(baby);
+// console.log(baby2);
+
+
+
+//Another Example
 class Parent{
     constructor(){
         this.fatherName = "Schwarzenegger";
     }
 }
-
 class Child extends Parent{
     constructor(name){
         super();
         this.name = name;
     }
+    getFullName(){
+        return this.name+ " " + this.fatherName;
+    }
 }
-const baby = new Child("Rahim");
+const baby = new Child("Arnold");
 const baby2 = new Child("Tom");
-console.log(baby);
+console.log(baby.getFullName());
 console.log(baby2);
 
 //=====End Inheritance====//
